330e4c659e852f3cde76358eed57aee573be689d
Mirko Bonadei <mirko.bonadei@gmail.com>
2015-02-07 17:08:13 +0100

    Introduced cell_sup to supervise cells

---
+++ src/cell_sup.erl
@@ -0,0 +1,35 @@
+-module(cell_sup).
+-behaviour(supervisor).
+
+-export([start_link/0, 
+         start_cell/3]).
+-export([init/1]).
+
+start_link() ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+-spec start_cell(cell:position(), cell:dimensions(), cell:content()) -> 
+                        supervisor:startchild_ret().
+start_cell(Position, Dimensions, InitialContent) ->
+    {ok, _Pid} = Res = supervisor:start_child(?MODULE,
+                                             child_spec(Position,
+                                                        Dimensions,
+                                                        InitialContent)),
+    Res.
+
+%%% OTP supervisor callback
+
+init([]) ->
+    RestartStrategy = one_for_one,
+    MaxRestarts = 1000,
+    MaxSecondsBetweenRestarts = 1,
+    SupFlags = {RestartStrategy, MaxRestarts, MaxSecondsBetweenRestarts},
+    {ok, {SupFlags, []}}.
+
+%%% private functions
+
+-spec child_spec(cell:position(), cell:dimensions(), cell:content()) -> 
+                        supervisor:child_spec().
+child_spec(Position, Dimensions, InitialContent) ->
+    {{cell, Position}, {cell, start_link, [Position, Dimensions, InitialContent]},
+     transient, infinity, worker, [cell]}.
