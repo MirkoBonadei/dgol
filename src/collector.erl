bb0e41ea9e6e08193d7f8636375a2b8bdf18ca96
Mirko Bonadei <mirko.bonadei@gmail.com>
2015-07-22 08:54:19 +0200

    Fix race-condition on collection
    We were thinking that no monitors left in the state of the collector
    means collection completed, but it is prone to a race condition when
    cell die.
    So now we ensure that the collection is finished checking an explicit
    counter.

--- src/collector.erl
+++ src/collector.erl
@@ -4,17 +4,18 @@
 -include_lib("eunit/include/eunit.hrl").
 
 -export([start_link/3]).
-
 -export([init/1,
          handle_call/3,
          handle_cast/2,
          handle_info/2,
          terminate/2,
          code_change/3]).
+
 -record(state, {time :: cell:time(),
                 neighbours_coordinates :: cell:neighbours(),
                 neighbours_alive :: non_neg_integer(),
                 neighbours_monitors :: [{cell:position(), reference()}],
+                neighbours_collected :: non_neg_integer(),
                 callback :: fun()}).
 -define(STARTING_TIMEOUT, 10).
 
@@ -25,6 +26,7 @@ start_link(TimeToCollect, Neighbours, Callback) ->
                                  neighbours_coordinates=Neighbours,
                                  neighbours_alive=0,
                                  neighbours_monitors=[],
+                                 neighbours_collected=0,
                                  callback=Callback},
                           []).
 
@@ -36,38 +38,39 @@ handle_call(_Request, _From, State) ->
     {reply, ok, State}.
 
 handle_cast(collect_cells, State) ->
-    lists:foreach(fun(Position) ->
-                          %% collector:collect_cell(self(), Position)
-                          gen_server:cast(self(), {collect_cell, Position})
-                  end, State#state.neighbours_coordinates),
+    lists:foreach(fun(Pos) -> collect_cell(self(), Pos) end, 
+                  State#state.neighbours_coordinates),
     {noreply, State};
-handle_cast({collect_cell, Position}, State) ->
-    case cell_locator:get(Position) of
+handle_cast({collect_cell, Pos}, State) ->
+    case cell_locator:get(Pos) of
         {error, not_found} ->
-            timer:apply_after(10, gen_server, cast, [self(), {collect_cell, Position}]),
+            timer:apply_after(10, gen_server, cast, [self(), {collect_cell, Pos}]),
             {noreply, State};
         Pid when is_pid(Pid) ->
             Self = self(),
             Callback = fun(Result) -> gen_server:cast(Self, Result) end,
             Ref = erlang:monitor(process, Pid),
             cell:eventually_get(Pid, State#state.time, Callback),
-            NewState = State#state{neighbours_monitors=[{Position, Ref}|State#state.neighbours_monitors]},
+            Monitors = [{Pos, Ref}|State#state.neighbours_monitors],
+            NewState = State#state{neighbours_monitors=Monitors},
             {noreply, NewState}
     end;
-handle_cast({cell, Position, Time, Content}, State) when Time =:= State#state.time ->
-    case lists:keyfind(Position, 1, State#state.neighbours_monitors) of
-        {Position, Reference} ->
-            NewNeighboursAlive = State#state.neighbours_alive + Content,
-            NewNeighboursMonitors = lists:subtract(State#state.neighbours_monitors,
-                                                      [{Position, Reference}]),
+handle_cast({cell, Pos, Time, Content}, State) when Time =:= State#state.time ->
+    case lists:keyfind(Pos, 1, State#state.neighbours_monitors) of
+        {Pos, Ref} ->
+            AliveCount = State#state.neighbours_alive + Content,
+            Monitors = lists:subtract(State#state.neighbours_monitors,
+                                      [{Pos, Ref}]),
+            CollectedCount = State#state.neighbours_collected + 1,
             
-            erlang:demonitor(Reference),
-            NewState = State#state{neighbours_monitors=NewNeighboursMonitors,
-                                   neighbours_alive=NewNeighboursAlive},
-            case NewNeighboursMonitors of
-                [] ->
+            erlang:demonitor(Ref),
+            NewState = State#state{neighbours_monitors=Monitors,
+                                   neighbours_alive=AliveCount,
+                                   neighbours_collected=CollectedCount},
+            case all_collected(NewState) of
+                true ->
                     Callback = State#state.callback,
-                    Callback(Time, NewNeighboursAlive),
+                    Callback(Time, AliveCount),
                     {stop, normal, NewState};
                 _ ->
                     {noreply, NewState}
@@ -78,12 +81,11 @@ handle_cast({cell, Position, Time, Content}, State) when Time =:= State#state.ti
 
 handle_info({'DOWN', Ref, process, _Pid, _Info}, State) ->
     case lists:keyfind(Ref, 2, State#state.neighbours_monitors) of
-        {Position, Ref} ->
-            NewNeighboursMonitors = lists:keydelete(Ref, 2, State#state.neighbours_monitors),
-            gen_server:cast(self(), {collect_cell, Position}),
-            {noreply, State#state{neighbours_monitors=NewNeighboursMonitors}};
-        _ ->
-            %% log or crash
+        {Pos, Ref} ->
+            Monitors = lists:keydelete(Ref, 2, State#state.neighbours_monitors),
+            gen_server:cast(self(), {collect_cell, Pos}),
+            {noreply, State#state{neighbours_monitors=Monitors}};
+        _ ->            
             {noreply, State}
     end.
 
@@ -93,6 +95,16 @@ terminate(_Reason, _State) ->
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
 
+%% Private functions
+-spec all_collected(#state{}) -> boolean().
+all_collected(#state{neighbours_coordinates=X, neighbours_collected=Y}) ->
+    length(X) =:= Y.
+
+-spec collect_cell(pid(), cell:position()) -> ok.
+collect_cell(CollectorPid, Position) ->
+    gen_server:cast(CollectorPid, {collect_cell, Position}).
+
+%% Tests
 -ifdef(TEST).
 
 start_dgol() ->
