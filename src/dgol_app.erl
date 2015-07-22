c29d00fdad0f9129f2be282b5708b33406b7211b
Mirko Bonadei <mirko.bonadei@gmail.com>
2015-07-22 08:59:15 +0200

    Remove useless comment

--- src/dgol_app.erl
+++ src/dgol_app.erl
@@ -2,11 +2,10 @@
 
 -behaviour(application).
 
--export([start/2
-        ,stop/1]).
+-export([start/2,
+         stop/1]).
 
 start(_StartType, _StartArgs) ->
-    %% TODO: is a check here useful or not (see OTP in Action page 124)
     dgol_sup:start_link().
 
 stop(_State) ->
