2c393c10c6bc04084529b5554a0b0088814bb900
gabriele.lana <gabriele.lana@cleancode.it>
2015-08-12 09:15:50 +0200

    WIP

--- src/cell_sup.erl
+++ src/cell_sup.erl
@@ -1,34 +1,25 @@
 -module(cell_sup).
 -behaviour(supervisor).
 
--export([start_link/0, 
-         start_cell/3]).
+-export([start_link/2,
+         start_cell/2]).
 -export([init/1]).
 
-start_link() ->
-    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+-spec start_link(cell:dimensions()) -> supervisor:startlink_ret().
+start_link(Dimensions) ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, [Dimensions]).
 
--spec start_cell(cell:position(), cell:dimensions(), cell:content()) -> 
-                        supervisor:startchild_ret().
-start_cell(Position, Dimensions, InitialContent) ->
-    {ok, _} = supervisor:start_child(?MODULE,
-                                     child_spec(Position,
-                                                Dimensions,
-                                                InitialContent)).
+-spec start_cell(cell:position(), cell:content()) -> supervisor:startchild_ret().
+start_cell(Position, InitialContent) ->
+    Cell = {{cell, Position}, {cell, start_link, [Position, InitialContent]},
+            transient, infinity, worker, [cell]},
+    {ok, _} = supervisor:start_child(?MODULE, Cell).
 
 %%% OTP supervisor callback
 
-init([]) ->
-    RestartStrategy = one_for_one,
-    MaxRestarts = 1000,
+init([{XDim, YDim}]) ->
+    RestartStrategy = simple_one_for_one,
+    MaxRestarts = XDim * YDim,
     MaxSecondsBetweenRestarts = 1,
     SupFlags = {RestartStrategy, MaxRestarts, MaxSecondsBetweenRestarts},
     {ok, {SupFlags, []}}.
-
-%%% private functions
-
--spec child_spec(cell:position(), cell:dimensions(), cell:content()) -> 
-                        supervisor:child_spec().
-child_spec(Position, Dimensions, InitialContent) ->
-    {{cell, Position}, {cell, start_link, [Position, Dimensions, InitialContent]},
-     transient, infinity, worker, [cell]}.
---
+++ src/universe_sup.erl
@@ -0,0 +1,23 @@
+-module(universe_sup).
+-behaviour(supervisor).
+-export([start_link/4]).
+-export([init/1]).
+
+-define(SERVER, ?MODULE).
+
+-spec start_link(pos_integer(), pos_integer(), [cell:position(), ...], [{module(), [term()]}]) ->
+                        supervisor:startlink_ret().
+start_link(XDim, YDim, InitialCells, EventHandlers) ->
+    {ok, _Pid} = supervisor:start_link({local, ?SERVER}, ?MODULE, [XDim, YDim, InitialCells, EventHandlers]).
+
+%%% OTP callbacks
+% TODO: remove EventHandlers
+init([XDim, YDim, InitialCells, _EventHandlers]) ->
+    RestartStrategy = {one_for_all, 0, 1},
+    CellLocator = {cell_locator, {cell_locator, start_link, []},
+                   permanent, 100, worker, [cell_locator]},
+    CellSup = {cell_sup, {cell_sup, start_link, {XDim, YDim}},
+               permanent, infinity, supervisor, [cell_sup]},
+    Universe = {universe, {universe, start_link, [{XDim, YDim}, InitialCells]},
+                permanent, infinity, worker, [universe]},
+    {ok, {RestartStrategy, [CellLocator, CellSup, Universe]}}.
